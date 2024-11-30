6ed7f5e7e13a269d1a9b7eeb21ec4b393338ef06
Kevin Heredia <kevin@ravecodesolutions.com>
2024-11-30 17:10:42 -0500

    tests arreglados

--- KCDragonBallProfTests/Mocks/URLProtocolMock.swift
+++ KCDragonBallProfTests/Mocks/URLProtocolMock.swift
@@ -6,27 +6,31 @@ class URLProtocolMock: URLProtocol {
     static var testData: Data?
     
     override class func canInit(with request: URLRequest) -> Bool {
-        return true
+        return true // Siempre intercepta las solicitudes
     }
-    
+
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
-    
+
     override func startLoading() {
         if let data = URLProtocolMock.testData {
-            client?.urlProtocol(self, didLoad: data)
+            self.client?.urlProtocol(self, didLoad: data)
+            self.client?.urlProtocolDidFinishLoading(self)
         }
-        client?.urlProtocolDidFinishLoading(self)
     }
-    
-    override func stopLoading() {}
+
+    override func stopLoading() {
+        // No se necesita hacer nada aquí
+    }
     
     static func startInterceptingRequests() {
         URLProtocol.registerClass(URLProtocolMock.self)
     }
-    
+
     static func stopInterceptingRequests() {
         URLProtocol.unregisterClass(URLProtocolMock.self)
     }
 }
+
+
--- KCDragonBallProfTests/UIIMageViewExtensionTests.swift
+++ KCDragonBallProfTests/UIIMageViewExtensionTests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import UIKit
 @testable import KCDragonBallProf
 
 class UIImageViewExtensionTests: XCTestCase {
@@ -8,6 +7,7 @@ class UIImageViewExtensionTests: XCTestCase {
         // Given
         let imageView = UIImageView()
         let expectation = self.expectation(description: "Imagen cargada correctamente")
+        
         guard let url = URL(string: "https://cdn.alfabetajuega.com/alfabetajuega/2020/08/Krilin.jpg?width=300") else {
             XCTFail("URL inválida")
             return
@@ -22,13 +22,18 @@ class UIImageViewExtensionTests: XCTestCase {
         URLProtocolMock.startInterceptingRequests()
         imageView.loadImageRemote(url: url)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        // Espera a que la imagen se cargue
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             // Then
+            // Verifica que la imagen no es nil
             XCTAssertNotNil(imageView.image, "La imagen no fue cargada en UIImageView")
-            expectation.fulfill()
+            expectation.fulfill() // Fulfills the expectation after the image is set
         }
         
-        wait(for: [expectation], timeout: 5)
+        // Espera un tiempo para que se complete la carga de la imagen
+        wait(for: [expectation], timeout: 3)
+        
+        // Detiene la interceptación de solicitudes
         URLProtocolMock.stopInterceptingRequests()
     }
 }
